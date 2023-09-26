7e1d8000bd717e9db9f39eabe4559c2a050a7bd2
Suhyun Jeong <thebbbb1206@gmail.com>
2023-09-26 23:54:24 +0900

    리팩토링+ 이슈 해결

--- app/src/main/java/kr/ac/duksung/dukbab/MenuDTO.java
+++ app/src/main/java/kr/ac/duksung/dukbab/MenuDTO.java
@@ -1,6 +1,9 @@
 package kr.ac.duksung.dukbab;
 
-public class MenuDTO {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MenuDTO implements Parcelable{
     private String name;
     private String price;
     private int imageResourceId;
@@ -15,11 +18,53 @@ public class MenuDTO {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPrice() {
         return price;
     }
 
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
     public int getImageResourceId() {
         return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    // Parcelable 구현 메서드
+    protected MenuDTO(Parcel in) {
+        name = in.readString();
+        price = in.readString();
+        imageResourceId = in.readInt();
+    }
+
+    public static final Parcelable.Creator<MenuDTO> CREATOR = new Parcelable.Creator<MenuDTO>() {
+        @Override
+        public MenuDTO createFromParcel(Parcel in) {
+            return new MenuDTO(in);
+        }
+
+        @Override
+        public MenuDTO[] newArray(int size) {
+            return new MenuDTO[size];
+        }
+    };
+
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(price);
+        dest.writeInt(imageResourceId);
     }
 }
--- app/src/main/java/kr/ac/duksung/dukbab/navigation/HomeFragment.java
+++ app/src/main/java/kr/ac/duksung/dukbab/navigation/HomeFragment.java
@@ -13,40 +13,48 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import kr.ac.duksung.dukbab.BunsikFragment;
-import kr.ac.duksung.dukbab.HaejangSoupFragment;
-import kr.ac.duksung.dukbab.HomemadeCutletFragment;
-import kr.ac.duksung.dukbab.JpFoodFragment;
-import kr.ac.duksung.dukbab.MaraTangFragment;
+import kr.ac.duksung.dukbab.MenuViewFragment;
 import kr.ac.duksung.dukbab.R;
-import kr.ac.duksung.dukbab.TodayMenuFragment;
 
 public class HomeFragment extends Fragment {
-    private TabLayout tablayout;
+    private TabLayout tabLayout;
     private ViewPager2 viewPager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tablayout = view.findViewById(R.id.tablayout);
+        tabLayout = view.findViewById(R.id.tabLayout);
         viewPager = view.findViewById(R.id.viewPager);
 
         // 탭 추가
-        tablayout.addTab(tablayout.newTab().setText("오늘의 메뉴"));
-        tablayout.addTab(tablayout.newTab().setText("마라탕"));
-        tablayout.addTab(tablayout.newTab().setText("분식"));
-        tablayout.addTab(tablayout.newTab().setText("수제돈까스"));
-        tablayout.addTab(tablayout.newTab().setText("해장국"));
-        tablayout.addTab(tablayout.newTab().setText("일식"));
+        tabLayout.addTab(tabLayout.newTab().setText("오늘의 메뉴"));
+        tabLayout.addTab(tabLayout.newTab().setText("마라탕"));
+        tabLayout.addTab(tabLayout.newTab().setText("분식"));
+        tabLayout.addTab(tabLayout.newTab().setText("수제돈까스"));
+        tabLayout.addTab(tabLayout.newTab().setText("해장국"));
+        tabLayout.addTab(tabLayout.newTab().setText("일식"));
+
+        // ViewPager에 어댑터 연결
+        MenuPagerAdapter pagerAdapter = new MenuPagerAdapter(requireActivity(), tabLayout);
+        viewPager.setAdapter(pagerAdapter);
+
+        // ViewPager2 페이지 변경 이벤트 감지
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                // ViewPager2 페이지가 변경될 때 탭을 선택한 것처럼 업데이트
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
 
         // TabLayout의 탭 선택 리스너 설정
-        tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
-
             }
 
             @Override
@@ -58,43 +66,30 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        // ViewPager에 어댑터 연결
-        MenuPagerAdapter pagerAdapter = new MenuPagerAdapter(requireActivity()); // FragmentActivity를 사용
-        viewPager.setAdapter(pagerAdapter);
-
         return view;
     }
 
     public static class MenuPagerAdapter extends FragmentStateAdapter {
+        private TabLayout tabLayout;
 
-        public MenuPagerAdapter(FragmentActivity activity) {
+        public MenuPagerAdapter(FragmentActivity activity, TabLayout tabLayout) {
             super(activity);
+            this.tabLayout = tabLayout;
         }
 
         @NonNull
         @Override
         public Fragment createFragment(int position) {
-            switch (position) {
-                case 0:
-                    return new TodayMenuFragment();
-                case 1:
-                    return new MaraTangFragment();
-                case 2:
-                    return new BunsikFragment();
-                case 3:
-                    return new HomemadeCutletFragment();
-                case 4:
-                    return new HaejangSoupFragment();
-                case 5:
-                    return new JpFoodFragment();
-                default:
-                    return null;
-            }
+            String selectedTabText = tabLayout.getTabAt(position).getText().toString();
+
+            return MenuViewFragment.newInstance(selectedTabText);
         }
 
         @Override
         public int getItemCount() {
-            return 6;
+            return tabLayout.getTabCount();
         }
     }
 }
+
+
