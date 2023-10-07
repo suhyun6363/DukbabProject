24deebcf3872a3c35739bf41f5918db78218d188
Suhyun Jeong <thebbbb1206@gmail.com>
2023-10-08 03:00:01 +0900

    옵션창 구현 완료, 장바구니 가격총합 DB에서 받아서 구현 할 예정

---
+++ app/src/main/java/kr/ac/duksung/dukbab/Home/OrderActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.duksung.dukbab.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
