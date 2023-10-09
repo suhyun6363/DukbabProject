f1ee2ac6076f57f5956729d831a7f9224541c618
yeeunAhn <yeeun780@gmail.com>
2023-10-09 22:37:37 +0900

    10092237

---
+++ app/src/main/java/kr/ac/duksung/dukbab/TodayMenuActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.duksung.dukbab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodayMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_menu);
+    }
+}
